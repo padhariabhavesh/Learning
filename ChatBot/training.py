5cdf4ee1e8ddb0a479f017f997e7ca6567c1ce57
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-09-11 22:49:15 +0530

    Update the code WordNetLemmatizer

--- ChatBot/training.py
+++ ChatBot/training.py
@@ -5,10 +5,11 @@ import json
 import pickle
 import numpy as np
 import nltk
-nltk.download('punkt')
+nltk.download('omw-1.4')
 import warnings
 warnings.filterwarnings('ignore')
 from nltk.stem import WordNetLemmatizer
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
@@ -29,4 +30,18 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 print(documents)
+print('*'* 30)
 
+
+wnl = WordNetLemmatizer()
+
+lemmatized_words = [[wnl.lemmatize(word) for word in l] for l in words]
+print(lemmatized_words)
+print('*'* 30)
+
+classes = sorted(set(classes))
+pickle.dump(words, open('lemmatized_words.pkl', 'wb'))
+pickle.dump(words, open('classess.pkl', 'wb'))
+
+# training = []
+# output_empty = [0] * len(classes)
