96844a2ad1b3d7274aa904b24b7674f59631af88
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-09-22 18:15:01 +0530

    Leetcode challenge 22/09/2022

---
+++ LeetCode Solutions/Reverse Words in a String III.py
@@ -0,0 +1,15 @@
+"""
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+"""
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        res = []
+
+        for word in words:
+            res.append(word[::-1])
+            res.append(" ")
+
+        return "".join(res).strip()
