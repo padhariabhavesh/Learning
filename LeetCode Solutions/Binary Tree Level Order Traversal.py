83c95042dea6f4ca60fa95fdd209cb044d2b65ed
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-09-25 19:30:28 +0530

    Level 1 Tree

---
+++ LeetCode Solutions/Binary Tree Level Order Traversal.py
@@ -0,0 +1,33 @@
+"""
+@Bhavesh Padharia
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        q, result = deque(), []
+        if root:
+            q.append(root)
+        while len(q):
+            level = []
+            for _ in range(len(q)):
+                x = q.popleft()
+                level.append(x.val)
+                if x.left:
+                    q.append(x.left)
+                if x.right:
+                    q.append(x.right)
+            result.append(level)
+        return result
