d2c2a92f48b264e8774c010390571fb9175a55b5
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-09-28 23:45:29 +0530

    Level 1 28/09/2022

---
+++ LeetCode Solutions/Flood Fill.py
@@ -0,0 +1,21 @@
+"""
+An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+
+Return the modified image after performing the flood fill.
+"""
+
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        rows, cols, orig_color = len(image), len(image[0]), image[sr][sc]
+        def traverse(row, col):
+            if (not (0 <= row < rows and 0 <= col < cols)) or image[row][col] != orig_color:
+                return
+            image[row][col] = newColor
+            [traverse(row + x, col + y) for (x, y) in ((0, 1), (1, 0), (0, -1), (-1, 0))]
+        if orig_color != newColor:
+            traverse(sr, sc)
+        return image
