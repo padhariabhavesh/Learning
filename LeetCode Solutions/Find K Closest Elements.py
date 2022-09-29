42496999bc262c15109d2de060195dfb0f10e1c1
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-09-29 18:25:05 +0530

    Challenge  29/09/2022

---
+++ LeetCode Solutions/Find K Closest Elements.py
@@ -0,0 +1,24 @@
+"""
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+
+"""
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r, = 0, len(arr) - k
+
+        while l < r:
+            m = (l + r) // 2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l:l + k]
+
+
