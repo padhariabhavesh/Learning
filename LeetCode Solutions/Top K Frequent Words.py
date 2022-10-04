09edeb8e7bfb33ea5be76ca9affb8b9504c396f6
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-10-04 23:43:04 +0530

    Level 1  04/10/2022

---
+++ LeetCode Solutions/Top K Frequent Words.py
@@ -0,0 +1,24 @@
+"""
+Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+"""
+
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        results = []
+
+        count = {}
+        for word in words:
+            count[word] = count.get(word, 0) + 1
+
+        heap = []
+        for key, value in count.items():
+            heapq.heappush(heap, (-value, key))
+
+        for i in range(k):
+            results.append(heapq.heappop(heap)[1])
+
+        return results
+
