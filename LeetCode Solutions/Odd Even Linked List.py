1e7d6ba2bd4229ce37de983b9e281618e242032a
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-10-08 23:49:31 +0530

    Level 2  08/10/2022

--- LeetCode Solutions/Odd Even Linked List.py
+++ LeetCode Solutions/Odd Even Linked List.py
@@ -9,3 +9,35 @@ You must solve the problem in O(1) extra space complexity and O(n) time complexi
 
 
 """
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+
+        odd_head = ListNode(0)
+        odd = odd_head
+
+        even_head = ListNode(0)
+        even = even_head
+
+        count = 0
+        while head != None:
+            if count % 2 == 0:
+                odd.next = head
+                odd = head
+            else:
+                even.next = head
+                even = head
+
+            count += 1
+            head = head.next
+
+        even.next = None
+        odd.next = even_head.next
+
+        return odd_head.next
