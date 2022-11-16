e5d98318c596fa5e91dbcbdefcc39e54c9ec1789
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-11-16 22:41:18 +0530

    Challenge 16/11/2022

--- LeetCode Solutions/November/Guess Number Higher or Lower.py
+++ LeetCode Solutions/November/Guess Number Higher or Lower.py
@@ -13,3 +13,17 @@ You call a pre-defined API int guess(int num), which returns three possible resu
 Return the number that I picked.
 """
 
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n, s=1):
+        m = (s + n) // 2
+        g = guess(m)
+        n -= (n - m + 1)*g*(g-1)//2     # in these two lines, g-1 and g+1 act as binary flags
+        s += (m - s + 1)*g*(g+1)//2     # for performing or not performing the change
+        return m if not g else self.guessNumber(n, s)
