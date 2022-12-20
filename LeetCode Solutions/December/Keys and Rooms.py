2309b8064fe18d2febb0a61ab4b301f6d0ba49df
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-12-20 10:30:17 +0530

    Challenge 20/12/2022

---
+++ LeetCode Solutions/December/Keys and Rooms.py
@@ -0,0 +1,39 @@
+"""
+There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. However, you cannot enter a locked room without having its key.
+
+When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+
+Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.
+
+
+"""
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code
+```
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        seen = [False] * len(rooms)
+        seen[0] = True
+        stack = [0]
+        #At the beginning, we have a todo list "stack" of keys to use.
+        #'seen' represents at some point we have entered this room.
+        while stack:  #While we have keys...
+            node = stack.pop() # get the next key 'node'
+            for nei in rooms[node]: # For every key in room # 'node'...
+                if not seen[nei]: # ... that hasn't been used yet
+                    seen[nei] = True # mark that we've entered the room
+                    stack.append(nei) # add the key to the todo list
+        return all(seen) # Return true iff we've visited every room
+```
