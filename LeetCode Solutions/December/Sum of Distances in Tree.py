ec0d191c01392e77e1df9a8aa84387c30b926194
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-12-22 08:35:05 +0530

    Challenge 21/12/2022

---
+++ LeetCode Solutions/December/Sum of Distances in Tree.py
@@ -0,0 +1,33 @@
+"""
+There is an undirected connected tree with n nodes labeled from 0 to n - 1 and n - 1 edges.
+
+You are given the integer n and the array edges where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
+
+Return an array answer of length n where answer[i] is the sum of the distances between the ith node in the tree and all other nodes.
+"""
+
+class Solution(object):
+    def sumOfDistancesInTree(self, N, edges):
+        graph = collections.defaultdict(set)
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        count = [1] * N
+        ans = [0] * N
+        def dfs(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    dfs(child, node)
+                    count[node] += count[child]
+                    ans[node] += ans[child] + count[child]
+
+        def dfs2(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    ans[child] = ans[node] - count[child] + N - count[child]
+                    dfs2(child, node)
+
+        dfs()
+        dfs2()
+        return ans
