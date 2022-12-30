c9b44084069904327bca7224ebe5146427e588a8
padhariabhavesh <padharia.bhavesh06@gmail.com>
2022-12-30 10:11:02 +0530

    Python 30/12/2022

---
+++ LeetCode Solutions/December/All Paths From Source to Target.py
@@ -0,0 +1,20 @@
+"""
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+"""
+
+class Solution:
+    def allPathsSourceTarget(self, graph):
+        def dfs(i, cur):
+            if i == nodes - 1:
+                result.append(cur[:])
+                return
+
+            for j in graph[i]:
+                dfs(j, cur + [j])
+
+        nodes = len(graph)
+        result = []
+        dfs(0, [0])
+        return result
