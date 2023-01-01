fdce8aa7cf3bf5ad15c4e8bd8c2abd9c3035369e
padhariabhavesh <padharia.bhavesh06@gmail.com>
2023-01-01 11:44:48 +0530

    Python 01/01/2023

---
+++ LeetCode Solutions/January/Word Pattern.py
@@ -0,0 +1,14 @@
+"""
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+"""
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+
+        s = s.split()
+
+        return (len(set(pattern)) ==
+                len(set(s)) ==
+                len(set(zip_longest(pattern,s))))
