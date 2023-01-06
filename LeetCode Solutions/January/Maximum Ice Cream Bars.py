c65e2428c25bb834d98690285ace3d906fddac0a
padhariabhavesh <padharia.bhavesh06@gmail.com>
2023-01-06 10:25:03 +0530

    Python 06/01/2023

---
+++ LeetCode Solutions/January/Maximum Ice Cream Bars.py
@@ -0,0 +1,23 @@
+"""
+It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+
+At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible.
+
+Return the maximum number of ice cream bars the boy can buy with coins coins.
+
+Note: The boy can buy the ice cream bars in any order.
+"""
+
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+
+        costs.sort()
+        res = 0
+        for price in costs:
+            if price <= coins:
+                res += 1
+                coins -= price
+            else:
+                break
+        return res
