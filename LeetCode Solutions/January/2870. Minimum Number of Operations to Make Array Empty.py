8fe88ecf9db2d45453fe8cfe5aa66f9d3167baa7
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-01-04 23:12:17 +0530

    Solution 2870

---
+++ LeetCode Solutions/January/2870. Minimum Number of Operations to Make Array Empty.py
@@ -0,0 +1,29 @@
+'''
+2870. Minimum Number of Operations to Make Array Empty
+
+You are given a 0-indexed array nums consisting of positive integers.
+
+There are two types of operations that you can apply on the array any number of times:
+
+    Choose two elements with equal values and delete them from the array.
+    Choose three elements with equal values and delete them from the array.
+
+Return the minimum number of operations required to make the array empty, or -1 if it is not possible.
+'''
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        from collections import Counter
+        mp = Counter(nums)
+        
+        count = 0
+        for t in mp.values():
+            if t == 1:
+                return -1
+            count += t // 3
+            if t % 3:
+                count += 1
+                
+        return count
+
+
