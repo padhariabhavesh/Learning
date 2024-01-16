7989308feebd28dbc8a2aadb19a0dcfd0305421f
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-01-16 06:58:58 +0530

    Solution 380

---
+++ LeetCode Solutions/January/380. Insert Delete GetRandom O(1).py
@@ -0,0 +1,55 @@
+'''
+380. Insert Delete GetRandom O(1)
+
+Implement the RandomizedSet class:
+
+    RandomizedSet() Initializes the RandomizedSet object.
+    bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+    bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+    int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+
+You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+Check Here: https://leetcode.com/problems/insert-delete-getrandom-o1/solutions/4572858/380-insert-delete-getrandom-o-1-python
+'''
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.lst = []
+        self.idx_map = {}
+
+    def search(self, val):
+        return val in self.idx_map
+
+    def insert(self, val):
+        if self.search(val):
+            return False
+
+        self.lst.append(val)
+        self.idx_map[val] = len(self.lst) - 1
+        return True
+
+    def remove(self, val):
+        if not self.search(val):
+            return False
+
+        idx = self.idx_map[val]
+        self.lst[idx] = self.lst[-1]
+        self.idx_map[self.lst[-1]] = idx
+        self.lst.pop()
+        del self.idx_map[val]
+        return True
+
+    def getRandom(self):
+        return random.choice(self.lst)
+
+
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
