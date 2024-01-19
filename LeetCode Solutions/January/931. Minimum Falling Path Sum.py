181d65d3f295a14154fe95e7a99545eef167db09
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-01-19 06:30:16 +0530

    Solutions 931

---
+++ LeetCode Solutions/January/931. Minimum Falling Path Sum.py
@@ -0,0 +1,48 @@
+'''
+931. Minimum Falling Path Sum
+
+
+Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+
+A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+'''
+
+class Solution:
+    def minFallingPathSum(self, A: List[List[int]]) -> int:
+        m, n = len(A), len(A[0])
+
+        if m == 1 or n == 1:
+            return A[0][0]
+
+        dp = [[float('inf')] * n for _ in range(m)]
+        ans = float('inf')
+
+        for i in range(len(A)):
+            ans = min(ans, self.minFallingPathSumHelper(A, 0, i, dp))
+
+        return ans
+
+    def minFallingPathSumHelper(self, A, row, col, dp):
+        m, n = len(A), len(A[0])
+
+        if dp[row][col] != float('inf'):
+            return dp[row][col]
+
+        if row == m - 1:
+            return A[row][col]
+
+        left = right = float('inf')
+
+        if col > 0:
+            left = self.minFallingPathSumHelper(A, row + 1, col - 1, dp)
+
+        straight = self.minFallingPathSumHelper(A, row + 1, col, dp)
+
+        if col < n - 1:
+            right = self.minFallingPathSumHelper(A, row + 1, col + 1, dp)
+
+        dp[row][col] = min(left, min(straight, right)) + A[row][col]
+
+        return dp[row][col]
+
+
