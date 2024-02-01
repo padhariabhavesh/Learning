dac60fee903d8da44f38ef7c8624835bbaebc5d4
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-02-01 06:22:39 +0530

    Solution 2966

---
+++ LeetCode Solutions/January/2966. Divide Array Into Arrays With Max Difference.py
@@ -0,0 +1,33 @@
+'''
+2966. Divide Array Into Arrays With Max Difference
+You are given an integer array nums of size n and a positive integer k.
+
+Divide the array into one or more arrays of size 3 satisfying the following conditions:
+
+    Each element of nums should be in exactly one array.
+    The difference between any two elements in one array is less than or equal to k.
+
+Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
+
+https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/solutions/4656994/2966-divide-array-into-arrays-with-max-difference-python 
+'''
+
+class Solution:
+    def divideArray(self, nums, k):
+        size = len(nums)
+        if size % 3 != 0:
+            return []
+
+        nums.sort()
+
+        result = []
+        group_index = 0
+        for i in range(0, size, 3):
+            if i + 2 < size and nums[i + 2] - nums[i] <= k:
+                result.append([nums[i], nums[i + 1], nums[i + 2]])
+                group_index += 1
+            else:
+                return []
+        return result
+
+
