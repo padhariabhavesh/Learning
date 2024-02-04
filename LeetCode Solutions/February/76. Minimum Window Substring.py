f57ea1427f396cdda132ca74ea69f5f138f2baa4
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-02-04 09:17:18 +0530

    Solution 76

---
+++ LeetCode Solutions/February/76. Minimum Window Substring.py
@@ -0,0 +1,53 @@
+'''
+
+76. Minimum Window Substring
+
+Given two strings s and t of lengths m and n respectively, return the minimum window
+substring
+of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+'''
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t:
+            return ""
+
+        dictT = defaultdict(int)
+        for c in t:
+            dictT[c] += 1
+
+        required = len(dictT)
+        l, r = 0, 0
+        formed = 0
+
+        windowCounts = defaultdict(int)
+        ans = [-1, 0, 0]
+
+        while r < len(s):
+            c = s[r]
+            windowCounts[c] += 1
+
+            if c in dictT and windowCounts[c] == dictT[c]:
+                formed += 1
+
+            while l <= r and formed == required:
+                c = s[l]
+
+                if ans[0] == -1 or r - l + 1 < ans[0]:
+                    ans[0] = r - l + 1
+                    ans[1] = l
+                    ans[2] = r
+
+                windowCounts[c] -= 1
+                if c in dictT and windowCounts[c] < dictT[c]:
+                    formed -= 1
+
+                l += 1
+
+            r += 1
+
+        return "" if ans[0] == -1 else s[ans[1]:ans[2] + 1]
+
