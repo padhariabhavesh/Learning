6e2b9891a46dc4df6c70d0957afc0fbdb001ba6e
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-02-16 06:44:30 +0530

    Solutions 1481

---
+++ LeetCode Solutions/February/1481. Least Number of Unique Integers after K Removals.py
@@ -0,0 +1,20 @@
+'''
+1481. Least Number of Unique Integers after K Removals
+
+Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+'''
+
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        freq_map = Counter(arr)
+
+        sorted_freq = sorted(freq_map.items(), key=lambda x: x[1])
+
+        for num, freq in sorted_freq:
+            if k >= freq:
+                k -= freq
+                del freq_map[num]
+            else:
+                break
+
+        return len(freq_map)
