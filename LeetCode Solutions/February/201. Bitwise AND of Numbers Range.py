c1d1d0466edee9fa40e455ba86224c60ba3c9ceb
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-02-21 07:26:02 +0530

    Solution 201

---
+++ LeetCode Solutions/February/201. Bitwise AND of Numbers Range.py
@@ -0,0 +1,15 @@
+'''
+201. Bitwise AND of Numbers Range
+
+Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+'''
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        cnt = 0
+        while left != right:
+            left >>= 1
+            right >>= 1
+            cnt += 1
+        return left << cnt
+
