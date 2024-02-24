2781ad013abebfa3f45ae86894f37e26950fa44f
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-02-24 06:34:59 +0530

    Solution 2092

---
+++ LeetCode Solutions/February/2092. Find All People With Secret.py
@@ -0,0 +1,54 @@
+'''
+2092. Find All People With Secret
+
+You are given an integer n indicating there are n people numbered from 0 to n - 1. You are also given a 0-indexed 2D integer array meetings where meetings[i] = [xi, yi, timei] indicates that person xi and person yi have a meeting at timei. A person may attend multiple meetings at the same time. Finally, you are given an integer firstPerson.
+
+Person 0 has a secret and initially shares the secret with a person firstPerson at time 0. This secret is then shared every time a meeting takes place with a person that has the secret. More formally, for every meeting, if a person xi has the secret at timei, then they will share the secret with person yi, and vice versa.
+
+The secrets are shared instantaneously. That is, a person may receive the secret and share it with people in other meetings within the same time frame.
+
+Return a list of all the people that have the secret after all the meetings have taken place. You may return the answer in any order.
+
+URL: https://leetcode.com/problems/find-all-people-with-secret/solutions/4773936/2092-find-all-people-with-secret-python
+
+'''
+
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        known_set = set([0, firstPerson])
+        
+        sorted_meetings = []
+        meetings.sort(key=lambda x:x[2])
+
+        seen_time = set()
+        
+        for meeting in meetings:
+            if meeting[2] not in seen_time:
+                seen_time.add(meeting[2])
+                sorted_meetings.append([])
+            sorted_meetings[-1].append((meeting[0], meeting[1]))
+
+        for meeting_group in sorted_meetings:
+            people_know_secret = set()
+            graph = defaultdict(list)
+            
+            for p1, p2 in meeting_group:
+                graph[p1].append(p2)
+                graph[p2].append(p1)
+                if p1 in known_set:
+                    people_know_secret.add(p1)
+                if p2 in known_set:
+                    people_know_secret.add(p2)
+                    
+            queue = deque((people_know_secret))
+        
+            while queue:
+                curr = queue.popleft()
+                known_set.add(curr)
+                for neighbor in graph[curr]:
+                    if neighbor not in known_set:
+                        known_set.add(neighbor)
+                        queue.append(neighbor)
+
+        return list(known_set)
+
