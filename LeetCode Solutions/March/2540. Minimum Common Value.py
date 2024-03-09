f704753485b94689cde0a1fc2f174d5694f03076
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-03-09 07:53:15 +0530

    Solution 2540

---
+++ LeetCode Solutions/March/2540. Minimum Common Value.py
@@ -0,0 +1,27 @@
+'''
+2540. Minimum Common Value
+
+
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+https://leetcode.com/problems/minimum-common-value/solutions/4845391/2540-minimum-common-value-python
+'''
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i = 0
+        j = 0
+        common = float('inf')
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                common = nums1[i]
+                break
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        
+        return common if common != float('inf') else -1
