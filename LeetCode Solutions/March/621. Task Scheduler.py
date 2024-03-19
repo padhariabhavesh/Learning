faf5e9f32e2e17941efc6444fab6942822a80f8c
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-03-19 07:01:36 +0530

    Solutions 621

---
+++ LeetCode Solutions/March/621. Task Scheduler.py
@@ -0,0 +1,24 @@
+'''
+
+621. Task Scheduler
+
+You are given an array of CPU tasks, each represented by letters A to Z, and a cooling time, n. Each cycle or interval allows the completion of one task. Tasks can be completed in any order, but there's a constraint: identical tasks must be separated by at least n intervals due to cooling time.
+
+​Return the minimum number of intervals required to complete all tasks.
+
+https://leetcode.com/problems/task-scheduler/solutions/4895052/621-task-scheduler-python
+'''
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = [0] * 26
+        for task in tasks:
+            freq[ord(task) - ord('A')] += 1
+        freq.sort()
+        chunk = freq[25] - 1
+        idle = chunk * n
+
+        for i in range(24, -1, -1):
+            idle -= min(chunk, freq[i])
+
+        return len(tasks) + idle if idle >= 0 else len(tasks)
