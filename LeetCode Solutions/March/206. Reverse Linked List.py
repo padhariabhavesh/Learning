23aa947b685b2e3bdb50873dc2804af93961768f
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-03-21 07:25:44 +0530

    Solution 206

---
+++ LeetCode Solutions/March/206. Reverse Linked List.py
@@ -0,0 +1,27 @@
+'''
+
+206. Reverse Linked List
+
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+https://leetcode.com/problems/reverse-linked-list/solutions/4904062/206-reverse-linked-list-python
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev_node = None
+        current_node = head
+
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+
+        return prev_node
