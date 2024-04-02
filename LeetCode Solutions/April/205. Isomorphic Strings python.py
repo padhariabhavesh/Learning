2165d54037f76462534d7f362f7227ea82385c48
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-04-02 07:03:47 +0530

    Solution 205

---
+++ LeetCode Solutions/April/205. Isomorphic Strings python.py
@@ -0,0 +1,20 @@
+'''
+
+205. Isomorphic Strings python
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+https://leetcode.com/problems/isomorphic-strings/solutions/4960171/205-isomorphic-strings-python
+'''
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return len(set(zip(s,t))) == len(set(s)) == len(set(t))
