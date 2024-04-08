514d68e22c3aa0977cf49199a449c48e620b28d9
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-04-08 07:25:43 +0530

    Solution 1700

---
+++ LeetCode Solutions/April/1700. Number of Students Unable to Eat Lunch.py
@@ -0,0 +1,27 @@
+'''
+
+1700. Number of Students Unable to Eat Lunch
+
+The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively. All students stand in a queue. Each student either prefers square or circular sandwiches.
+
+The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches are placed in a stack. At each step:
+
+    If the student at the front of the queue prefers the sandwich on the top of the stack, they will take it and leave the queue.
+    Otherwise, they will leave it and go to the queue's end.
+
+This continues until none of the queue students want to take the top sandwich and are thus unable to eat.
+
+You are given two integer arrays students and sandwiches where sandwiches[i] is the type of the i​​​​​​th sandwich in the stack (i = 0 is the top of the stack) and students[j] is the preference of the j​​​​​​th student in the initial queue (j = 0 is the front of the queue). Return the number of students that are unable to eat.
+
+https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/solutions/4990422/1700-number-of-students-unable-to-eat-lunch-python
+'''
+
+class Solution:
+    def countStudents(self, st: List[int], sa: List[int]) -> int:
+        ct = Counter(st)
+        for s in sa:
+            if ct[s] > 0: 
+                ct[s] -= 1
+            else:
+                return ct.total()
+        return 0
