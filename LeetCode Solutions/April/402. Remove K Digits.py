8cffcde9d719f29a3346a65a49ef33162779b70b
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-04-11 22:12:02 +0530

    Solution 402

---
+++ LeetCode Solutions/April/402. Remove K Digits.py
@@ -0,0 +1,27 @@
+'''
+
+402. Remove K Digits
+
+Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+https://leetcode.com/problems/remove-k-digits/solutions/5008793/402-remove-k-digits-python
+'''
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        
+        for digit in num:
+            while stack and k > 0 and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+        
+        # If k > 0, remove remaining k digits from the end of the stack
+        stack = stack[:-k] if k > 0 else stack
+        
+        # Remove leading zeros
+        result = ''.join(stack).lstrip('0')
+        
+        # Handle edge case where result might be empty
+        return result if result else '0'                
