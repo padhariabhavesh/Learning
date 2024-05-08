283ff32765c05e39a5463ed4a8e6725405883f20
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-05-08 21:37:08 +0530

    Solution

---
+++ LeetCode Solutions/May/506. Relative Ranks.py
@@ -0,0 +1,25 @@
+'''
+
+506. Relative Ranks
+
+
+You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+    The 1st place athlete's rank is "Gold Medal".
+    The 2nd place athlete's rank is "Silver Medal".
+    The 3rd place athlete's rank is "Bronze Medal".
+    For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+https://leetcode.com/problems/relative-ranks/solutions/5130962/506-relative-ranks-python
+ 
+'''
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        sorted_score = sorted(score, reverse=True)
+        medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        rank_mapping = {score: medals[i] if i < 3 else str(i + 1) for i, score in enumerate(sorted_score)}
+        return [rank_mapping[score] for score in score]
