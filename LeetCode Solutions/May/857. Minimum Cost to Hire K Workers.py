bd9e60a7eea78f8d57f36002d217d95cd2ac4064
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-05-11 18:48:22 +0530

    Solution 857

---
+++ LeetCode Solutions/May/857. Minimum Cost to Hire K Workers.py
@@ -0,0 +1,38 @@
+'''
+
+857. Minimum Cost to Hire K Workers
+
+There are n workers. You are given two integer arrays quality and wage where quality[i] is the quality of the ith worker and wage[i] is the minimum wage expectation for the ith worker.
+
+We want to hire exactly k workers to form a paid group. To hire a group of k workers, we must pay them according to the following rules:
+
+    Every worker in the paid group must be paid at least their minimum wage expectation.
+    In the group, each worker's pay must be directly proportional to their quality. This means if a worker’s quality is double that of another worker in the group, then they must be paid twice as much as the other worker.
+
+Given the integer k, return the least amount of money needed to form a paid group satisfying the above conditions. Answers within 10-5 of the actual answer will be accepted.
+
+'''
+
+class Solution:
+    def mincostToHireWorkers(
+        self, quality: List[int], wage: List[int], k: int
+    ) -> float:
+        ratio = sorted([(w / q, q) for w, q in zip(wage, quality)])
+        max_heap = []
+        quality_sum = 0
+        max_ratio = 0.0
+        
+        for i in range(k):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum += ratio[i][1]
+            heapq.heappush(max_heap, -ratio[i][1])
+        
+        res = max_ratio * quality_sum
+        
+        for i in range(k, len(quality)):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum += ratio[i][1] + heapq.heappop(max_heap)
+            heapq.heappush(max_heap, -ratio[i][1])
+            res = min(res, max_ratio * quality_sum)
+        
+        return res
