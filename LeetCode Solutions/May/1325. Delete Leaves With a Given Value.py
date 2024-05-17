e339947f4d073844463c81b2f1b88709f747c6fe
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2024-05-17 21:33:39 +0530

    Solutions 1325

---
+++ LeetCode Solutions/May/1325. Delete Leaves With a Given Value.py
@@ -0,0 +1,34 @@
+'''
+
+1325. Delete Leaves With a Given Value
+
+
+Given a binary tree root and an integer target, delete all the leaf nodes with value target.
+
+Note that once you delete a leaf node with value target, if its parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you cannot).
+
+
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def removeLeafNodes(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+        if not root.left and not root.right and root.val == target:
+            return None
+        return root
